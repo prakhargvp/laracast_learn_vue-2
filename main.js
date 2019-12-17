5687d50384d95b17160935098f0ab52f7ff5d089
Prakhar Agarwal <prakhargvp@gmail.com>
2019-12-17 11:05:49 +0530

    Adding Component task inside task-list

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+Vue.component('task-list', {
+	template: `
+		<div>
+			<task v-for="task in tasks">{{ task.description }}</task>
+		</div>`,
+	data() {
+		return {
+			tasks: [
+				{ description: 'Go to the gym', completed: true },
+				{ description: 'Go to ofice', completed: true },
+				{ description: 'Finish story points ', completed: true },
+				{ description: 'Go for table tennis', completed: false },
+				{ description: 'Go to the dinner', completed: false },
+				{ description: 'Read a Book', completed: false },
+			]
+		}
+	}
+});
+
 Vue.component('task', {
 	template: '<li><slot></slot></li>'
 });
